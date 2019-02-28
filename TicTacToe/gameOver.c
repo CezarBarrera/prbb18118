966843af348f71c0bbeeeba1e70199f7e66b5066
CezarBarrera <jackroadtrinity@gmail.com>
2019-02-28 17:10:05 +0300

    Homework

--- TicTacToe/gameOver.c
+++ TicTacToe/gameOver.c
@@ -3,44 +3,30 @@
 
 extern int notGameOver;
 extern int input;
-extern int field[];
+extern int field[FIELD_SIZE];
 extern int winner;
 
 
 void checkGameOver() {
-	// Check horizontals
-	if (field[0] == field[1] && field[1] == field[2]) {
-		if (field[0] == 'X') winner = 1;
-		else winner = 2;
-	}
-	if (field[3] == field[4] && field[4] == field[5]) {
-		if (field[3] == 'X') winner = 1;
-		else winner = 2;
-	}
-	if (field[6] == field[7] && field[7] == field[8]) {
-		if (field[6] == 'X') winner = 1;
-		else winner = 2;
-	}
-	// Check verticals
-	if (field[0] == field[3] && field[3] == field[6]) {
-		if (field[0] == 'X') winner = 1;
-		else winner = 2;
-	}
-	if (field[1] == field[4] && field[4] == field[7]) {
-		if (field[1] == 'X') winner = 1;
-		else winner = 2;
-	}
-	if (field[2] == field[5] && field[5] == field[8]) {
-		if (field[2] == 'X') winner = 1;
-		else winner = 2;
-	}
-	// Check diagonals
-	if ((field[0] == field[4] && field[4] == field[8]) ||
-		(field[2] == field[4] && field[4] == field[6])) {
-		if (field[4] == 'X') winner = 1;
-		else winner = 2;
-	}
-	if (!winner) {
+	for (int i = 0; i < FIELD_SIZE; ++i) {
+		if ((field[1] == field[2] && field[2] == field[3])
+			|| (field[3] == field[4] && field[4] == field[5])
+			|| (field[6] == field[7] && field[7] == field[8])) {
+			if (field[i] == 'X') winner = 1;
+			else winner = 2;
+		}
+		if (field[1] == field[4] && field[4] == field[7]
+			|| field[2] == field[5] && field[5] == field[8]
+			|| field[3] == field[6] && field[6] == field[9]) {
+			if (field[1] == 'X') winner = 1;
+			else winner = 2;
+		}
+		if ((field[1] == field[4] && field[4] == field[8]) ||
+			(field[2] == field[4] && field[4] == field[6])) {
+			if (field[4] == 'X') winner = 1;
+			else winner = 2;
+		}
+        if (!winner) {
 		for (int i = 0; i < FIELD_SIZE; i++) {
 			if (field[i] == i + 1) {
 				return;
@@ -48,4 +34,5 @@ void checkGameOver() {
 		}
 		winner = 3;
 	}
+	}	
 }
